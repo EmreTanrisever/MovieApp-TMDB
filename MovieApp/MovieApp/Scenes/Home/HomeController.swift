d099e453c10622bac8978b91e6f5c23f49bbc4fd
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-02-18 17:53:57 +0300

    Finish TabBar

--- MovieApp/MovieApp/Scenes/Home/HomeController.swift
+++ MovieApp/MovieApp/Scenes/Home/HomeController.swift
@@ -9,8 +9,41 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    private let leftView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "homeleftview")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.zPosition = 0
+        return view
+    }()
+    
+    private let nowPlayingLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configure()
+    }
+}
+
+extension HomeController {
+    func configure() {
         self.view.backgroundColor = .white
+        view.addSubviews(leftView, nowPlayingLabel)
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            leftView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            leftView.topAnchor.constraint(equalTo: view.topAnchor),
+            leftView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            leftView.widthAnchor.constraint(lessThanOrEqualToConstant: 133)
+        ])
     }
 }
