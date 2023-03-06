6e101973db27345f3dd5f54f7b8993fd5918a7a6
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-07 00:55:11 +0300

    Add some functions

--- MovieApp/MovieApp/Network/NetworkService.swift
+++ MovieApp/MovieApp/Network/NetworkService.swift
@@ -9,6 +9,9 @@ import Foundation
 
 protocol NetworkServiceProtocol {
     func getNowPlayingMovies(completion: @escaping(Swift.Result<Movies, Error>) -> Void)
+    func getPopularMovies(completion: @escaping(Result<Movies, Error>) -> Void)
+    func getTopRatedMovies(comletion: @escaping(Result<Movies, Error>) -> Void)
+    func getUpComingMovies(comletion: @escaping(Result<Movies, Error>) -> Void)
 }
 
 final class NetworkService: NetworkServiceProtocol {
@@ -24,6 +27,20 @@ final class NetworkService: NetworkServiceProtocol {
         guard let urlRequest = PopularMoviesRequest.shared.createURL() else { return }
         NetworkManager.shared.sendRequest(urlRequest: urlRequest) { response in
             completion(response)
+        }
+    }
+    
+    func getTopRatedMovies(comletion: @escaping(Result<Movies, Error>) -> Void) {
+        guard let urlRequest = TopRatedMoviesRequest.shared.createURL() else { return }
+        NetworkManager.shared.sendRequest(urlRequest: urlRequest) { response in
+            comletion(response)
+        }
+    }
+    
+    func getUpComingMovies(comletion: @escaping(Result<Movies, Error>) -> Void) {
+        guard let urlRequest = UpComingMoviesRequest.shared.createURL() else { return }
+        NetworkManager.shared.sendRequest(urlRequest: urlRequest) { response in
+            comletion(response)
         }
     }
 }
